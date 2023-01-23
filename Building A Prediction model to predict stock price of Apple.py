3b2d0f9a3d459f4b4fced7b4ccacc2c186607baa
dddesta <71279457+dddesta@users.noreply.github.com>
2023-01-23 13:06:18 -0500

    Update Building A Prediction model to predict stock price of Apple.py

--- Building A Prediction model to predict stock price of Apple.py
+++ Building A Prediction model to predict stock price of Apple.py
@@ -3,8 +3,6 @@
 
 # # Building A Prediction model to predict stock price of Apple.
 
-# In[1]:
-
 
 import pandas as pd
 import numpy as np
@@ -23,32 +21,17 @@ from keras.layers import LSTM,Dropout,Dense
 from sklearn.preprocessing import MinMaxScaler
 
 
-# In[2]:
-
-
 # Load the data
 df = pd.read_csv('HistoricalData_1641407819284.csv')
-
-
-# In[3]:
-
 
 df.head()
 
 
 # # Data Manipulation
-
-# In[4]:
-
-
 # Keep only the necessary columns
 
 df = df[['Date', 'Close/Last']]
 df.head()
-
-
-# In[5]:
-
 
 # Convert the closing price to a float and the date to a datetime
 df = df.replace({'\$':''}, regex = True)
@@ -56,27 +39,16 @@ df = df.astype({"Close/Last": float})
 df["Date"] = pd.to_datetime(df.Date, format="%m/%d/%Y")
 df.dtypes
 
-
-# In[6]:
-
-
 # Set the date as the index
 df.index = df['Date']
 df
 
-
 # # Data Visualization
-
-# In[7]:
-
 
 plt.plot(df["Close/Last"],label='Close Price history')
 
 
 # # Data Preparation
-
-# In[8]:
-
 
 # Sort the data by date
 df = df.sort_index(ascending=True,axis=0)
@@ -90,9 +62,6 @@ data.head()
 
 
 # ## Min-Max Scaler
-
-# In[12]:
-
 
 # Split the data into training and validation sets
 train_data = data[:200]
@@ -110,10 +79,6 @@ for i in range(60, len(train_data)):
 x_train_data, y_train_data = np.array(x_train_data), np.array(y_train_data)
 x_train_data = np.reshape(x_train_data, (x_train_data.shape[0], x_train_data.shape[1], 1))
 
-
-# In[13]:
-
-
 # Create the LSTM model
 model = Sequential()
 model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train_data.shape[1],1)))
@@ -127,9 +92,6 @@ model.fit(x_train_data, y_train_data, epochs=100, batch_size=64)
 
 # # Train and Test Data
 
-# In[14]:
-
-
 #Get the predicted closing prices
 inputs = data['Close/Last'][200:].to_numpy()
 inputs = np.reshape(inputs,(-1, 1))
@@ -139,17 +101,11 @@ X_test = inputs.reshape(inputs.shape[0],1)
 
 # # Predicted Function
 
-# In[16]:
-
-
 closing_price = model.predict(X_test)
 closing_price = scaler.inverse_transform(closing_price)
 
 
 # # Prediction Result
-
-# In[17]:
-
 
 # Plot the results
 valid_data['Predictions'] = closing_price
@@ -157,9 +113,6 @@ plt.plot(train_data['Close/Last'])
 plt.plot(valid_data[['Close/Last','Predictions']])
 plt.legend(['Train', 'Val', 'Prediction'], loc='upper left')
 plt.show()
-
-
-# In[18]:
 
 
 from sklearn.metrics import mean_absolute_error, mean_squared_error
@@ -174,8 +127,6 @@ print("MSE: ", mse)
 
 # # EXTRA
 
-# In[24]:
-
 
 print("Shape of x_train_data: ", np.shape(x_train_data))
 print("Shape of y_train_data: ", np.shape(y_train_data))
@@ -187,10 +138,6 @@ x_train_data = np.reshape(x_train_data, (x_train_data.shape[0],x_train_data.shap
 #Compile and fit the LSTM
 lstm_model.compile(loss='mean_squared_error', optimizer='adam')
 lstm_model.fit(x_train_data, y_train_data, epochs=100, batch_size=64)
-
-
-# In[25]:
-
 
 lstm_model.summary()
 
